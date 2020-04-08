c25c83c165d8ec14efb93f95f743859a8299af2f
Alexis <alexis.aubineau@ynov.com>
2020-04-08 09:04:15 +0200

    create projectile file and function & update vcxproj.filter

---
+++ SFML Tries/Projectile.cpp
@@ -0,0 +1,21 @@
+#include "Projectile.h"
+
+
+Projectile::Projectile()
+{
+	
+}
+
+Projectile::~Projectile()
+{
+}
+
+void Projectile::Draw(sf::RenderWindow& rw)
+{
+	VisibleGameObject::Draw(rw);
+}
+
+void Projectile::Update(float elapsedTime)
+{
+	
+}
